96d22ae187057f0153a0c14747e8aa4c981b3527
termine1 <gsafdq.tn.qe12@gmail.com>
2026-03-09 11:21:57 +0700

    Корневые слова. Сделал автоматически выбор теста от 1-4 Выбираю номер теста, соединяю номер и название файла и открываю этот файл через ifstream Далее создаю пустой массив для хранения слов Читаю слова через цикл и кладу в массив word push_back - добавляет в конец списка слово word - временно хранит переменную и потом закидывает в список words

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -1,8 +1,39 @@
 ﻿
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Введите номер теста: \n"; 
+	int num;
+	cin >> num;
+	string test = "test" + to_string(num) + ".txt";
+
+	ifstream filename(test);
+
+
+    vector<string> words;
+    string word;
+
+    while (filename >> word) {
+        words.push_back(word);
+    }
+
+    filename.close();
+
+ 
+    cout << "Слова:" << endl;
+    for (int i = 0; i < words.size(); i++) {
+        cout << i + 1 << ". " << words[i] << endl;
+    }
+	
+
+
+
 }
 
